2ca070831ca86d8080e168afac79f88d86635b12
Erick Caceres <katomax1@gmail.com>
2022-06-23 19:03:06 -0500

    modal hidden

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ let modal = document.querySelector('app-modal')
 let abrir = document.querySelector('#open')
 let cerrar = document.querySelector('#close')
 
-abrir.addEventListener('click', (e) => {
-  e.preventDefault()
-  modal.open()
-})
+// abrir.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   modal.open()
+// })
 
-cerrar.addEventListener('click', (e) => {
-  e.preventDefault()
-  modal.close()
-})
+// cerrar.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   modal.close()
+// })
